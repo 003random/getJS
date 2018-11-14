182ac41bb06787600269fa578e0407e7e29e6ffb
003random <003random@protonmail.com>
2018-11-15 00:32:33 +0100

    Updated the arguments

--- main.go
+++ main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-        "bufio"
-        "flag"
-        "fmt"
-        "net/http"
-        "net/url"
-        "os"
-        "strings"
+	"bufio"
+	"flag"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
 	"strconv"
+	"strings"
 
-        "github.com/PuerkitoBio/goquery"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/logrusorgru/aurora"
 )
 
 type logger interface {
-        Log(msg string)
+	Log(msg string)
 	Error(msg string, err error)
 }
 
@@ -27,8 +27,7 @@ func (s silent) Log(msg string) {
 func (s silent) Error(msg string, err error) {
 }
 
-type verbose struct{
-	
+type verbose struct {
 }
 
 func (v verbose) Log(msg string) {
@@ -36,10 +35,10 @@ func (v verbose) Log(msg string) {
 }
 
 func Log(l logger, msg string) {
-        l.Log(msg)
+	l.Log(msg)
 }
 
-func (v verbose) Error(msg string, err error) {	
+func (v verbose) Error(msg string, err error) {
 	fmt.Fprintln(os.Stderr, au.Red(msg))
 	if err != nil {
 		fmt.Fprintln(os.Stderr, au.Red("[!] Error: "), au.Red(err))
@@ -59,212 +58,209 @@ func main() {
 	inputFileArg := flag.String("input", "", "Input file with urls")
 	resolveArg := flag.Bool("resolve", false, "Output only existing files")
 	completeArg := flag.Bool("complete", false, "Complete the url. e.g. append the domain to the path")
-	plainArg := flag.Bool("plain", false, "Output only the results")
-	silentArg := flag.Bool("silent", false, "Dont output anything")
+	verboseArg := flag.Bool("verbose", false, "Display info of what is going on")
 	noColorsArg := flag.Bool("nocolors", false, "Enable or disable colors")
 	flag.Parse()
 
 	au = aurora.NewAurora(!*noColorsArg)
 
-        var urls []string
-        var allSources []string
+	var urls []string
+	var allSources []string
 
-        if *silentArg || *plainArg {
-                output = silent{}
-        } else {
-                output = verbose{}
-        }
+	output = silent{}
 
-        stat, err := os.Stdin.Stat()
-        if err != nil {
-                output.Error("[!] Couldnt read Stdin", err)
-        }
+	if *verboseArg {
+		output = verbose{}
+	}
 
-        if (stat.Mode() & os.ModeCharDevice) == 0 {
-                scanner := bufio.NewScanner(os.Stdin)
-                for scanner.Scan() {
-                        urls = append(urls, scanner.Text())
-                }
-                if err := scanner.Err(); err != nil {
-                        output.Error("[!] Couldnt read Stdin", err)
-                }
-                if len(urls) > 0 {
-                        output.Log("[+] Received urls from Stdin")
-                }
-        }
+	stat, err := os.Stdin.Stat()
+	if err != nil {
+		output.Error("[!] Couldnt read Stdin", err)
+	}
 
-        if *inputFileArg != "" {
-                lines, err := readLines(*inputFileArg)
-                if err != nil {
-                        output.Error("[!] Couldn't read from input file", err)
-                }
-                output.Log("[+] Set url file to " + *inputFileArg)
-                urls = append(urls, lines...)
-        }
+	if (stat.Mode() & os.ModeCharDevice) == 0 {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			urls = append(urls, scanner.Text())
+		}
+		if err := scanner.Err(); err != nil {
+			output.Error("[!] Couldnt read Stdin", err)
+		}
+		if len(urls) > 0 {
+			output.Log("[+] Received urls from Stdin")
+		}
+	}
 
-        if *urlArg != "" {
-                output.Log("[+] Set url to " + *urlArg)
-                urls = append(urls, *urlArg)
-        }
+	if *inputFileArg != "" {
+		lines, err := readLines(*inputFileArg)
+		if err != nil {
+			output.Error("[!] Couldn't read from input file", err)
+		}
+		output.Log("[+] Set url file to " + *inputFileArg)
+		urls = append(urls, lines...)
+	}
 
-        if len(urls) == 0 {
-                output.Error("[!] No urls supplied", nil)
-                os.Exit(3)
-        }
+	if *urlArg != "" {
+		output.Log("[+] Set url to " + *urlArg)
+		urls = append(urls, *urlArg)
+	}
 
-        if *resolveArg && !*completeArg {
-                output.Error("[!] Resolve can only be used in combination with -complete", nil)
-                os.Exit(3)
-        }
+	if len(urls) == 0 {
+		output.Error("[!] No urls supplied", nil)
+		os.Exit(3)
+	}
 
-        for _, e := range urls {
-                var sourcesBak []string
-                var completedSuccessfully = true
-                output.Log("[+] Getting sources from " + e)
-                sources, err := getScriptSrc(e)
-                if err != nil {
-                        output.Error("[!] Couldn't get sources from " + e, err)
-                }
+	if *resolveArg && !*completeArg {
+		output.Error("[!] Resolve can only be used in combination with -complete", nil)
+		os.Exit(3)
+	}
 
-                if *completeArg {
-                        output.Log("[+] Completing URLs")
-                        sourcesBak = sources
-                        sources, err = completeUrls(sources, e)
-                        if err != nil {
-                                output.Error("[!] Couldn't complete URLs", err)
-                                sources = sourcesBak
-                                completedSuccessfully = false
-                        }
-                }
+	for _, e := range urls {
+		var sourcesBak []string
+		var completedSuccessfully = true
+		output.Log("[+] Getting sources from " + e)
+		sources, err := getScriptSrc(e)
+		if err != nil {
+			output.Error("[!] Couldn't get sources from "+e, err)
+		}
 
-                if *resolveArg && *completeArg {
-                        if completedSuccessfully {
-                                output.Log("[+] Resolving files")
-                                sourcesBak = sources
-                                sources, err = resolveUrls(sources)
-                                if err != nil {
-                                        output.Error("[!] Couldn't resolve URLs", err)
-                                        sources = sourcesBak
-                                }
-                        } else {
-                                output.Error("[!] Couldn't resolve URLs", nil)
-                        }
-                } else if *resolveArg {
-                        output.Error("[!] Resolve can only be used in combination with -complete", nil)
-                }
+		if *completeArg {
+			output.Log("[+] Completing URLs")
+			sourcesBak = sources
+			sources, err = completeUrls(sources, e)
+			if err != nil {
+				output.Error("[!] Couldn't complete URLs", err)
+				sources = sourcesBak
+				completedSuccessfully = false
+			}
+		}
 
-                if !*silentArg {
-                        for _, i := range sources {
-                                fmt.Println(i)
-                        }
-                }
+		if *resolveArg && *completeArg {
+			if completedSuccessfully {
+				output.Log("[+] Resolving files")
+				sourcesBak = sources
+				sources, err = resolveUrls(sources)
+				if err != nil {
+					output.Error("[!] Couldn't resolve URLs", err)
+					sources = sourcesBak
+				}
+			} else {
+				output.Error("[!] Couldn't resolve URLs", nil)
+			}
+		} else if *resolveArg {
+			output.Error("[!] Resolve can only be used in combination with -complete", nil)
+		}
 
-                if *outputFileArg != "" {
-                        allSources = append(allSources, sources...)
-                }
+		for _, i := range sources {
+			fmt.Println(i)
+		}
 
-        }
+		if *outputFileArg != "" {
+			allSources = append(allSources, sources...)
+		}
 
-        // Save to file
-        if *outputFileArg != "" {
-                output.Log("[+] Saving output to " + *outputFileArg)
-                err := saveToFile(allSources, *outputFileArg)
-                if err != nil {
-                        output.Error("[!] Couldn't save to output file " + *outputFileArg, err)
-                }
-        }
+	}
+
+	// Save to file
+	if *outputFileArg != "" {
+		output.Log("[+] Saving output to " + *outputFileArg)
+		err := saveToFile(allSources, *outputFileArg)
+		if err != nil {
+			output.Error("[!] Couldn't save to output file "+*outputFileArg, err)
+		}
+	}
 
 }
 
 // ToDO: Use channel instead of slide, and use io.Writer instead of file path
 func saveToFile(sources []string, path string) error {
-        file, err := os.Create(path)
-        if err != nil {
-                return err
-        }
-        defer file.Close()
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
-        w := bufio.NewWriter(file)
-        for _, line := range sources {
-                fmt.Fprintln(w, line)
-        }
-        return w.Flush()
+	w := bufio.NewWriter(file)
+	for _, line := range sources {
+		fmt.Fprintln(w, line)
+	}
+	return w.Flush()
 }
 
 func getScriptSrc(url string) ([]string, error) {
-        // Request the HTML page.
-        res, err := http.Get(url)
-        if err != nil {
-                return nil, err
-        }
-        defer res.Body.Close()
-        if res.StatusCode != 200 {
-                output.Error("[!] " + url + " returned an " + strconv.Itoa(res.StatusCode) + " instead of an 200 OK", nil)
-                return nil, nil
-        }
+	// Request the HTML page.
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		output.Error("[!] "+url+" returned an "+strconv.Itoa(res.StatusCode)+" instead of an 200 OK", nil)
+		return nil, nil
+	}
 
-        // Load the HTML document
-        doc, err := goquery.NewDocumentFromReader(res.Body)
-        if err != nil {
-                return nil, err
-        }
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return nil, err
+	}
 
-        var sources []string
+	var sources []string
 
-        // Find the script tags, and get the src
-        doc.Find("script").Each(func(i int, s *goquery.Selection) {
-                src, _ := s.Attr("src")
-                if src != "" {
-                        sources = append(sources, src)
-                }
-        })
+	// Find the script tags, and get the src
+	doc.Find("script").Each(func(i int, s *goquery.Selection) {
+		src, _ := s.Attr("src")
+		if src != "" {
+			sources = append(sources, src)
+		}
+	})
 
-        return sources, nil
+	return sources, nil
 }
 
 // ToDo: Use io.Writer instead of a file path
 func readLines(path string) ([]string, error) {
-        file, err := os.Open(path)
-        if err != nil {
-                return nil, err
-        }
-        defer file.Close()
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
 
-        var lines []string
-        scanner := bufio.NewScanner(file)
-        for scanner.Scan() {
-                lines = append(lines, scanner.Text())
-        }
-        return lines, scanner.Err()
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
 }
 
 func resolveUrls(s []string) ([]string, error) {
-        for i := len(s) - 1; i >= 0; i-- {
-                resp, err := http.Get(s[i])
-                if err != nil {
-                        return nil, err
-                }
-                if resp.StatusCode != 200 && resp.StatusCode != 304 {
-                        s = append(s[:i], s[i+1:]...)
-                }
-        }
-        return s, nil
+	for i := len(s) - 1; i >= 0; i-- {
+		resp, err := http.Get(s[i])
+		if err != nil {
+			return nil, err
+		}
+		if resp.StatusCode != 200 && resp.StatusCode != 304 {
+			s = append(s[:i], s[i+1:]...)
+		}
+	}
+	return s, nil
 }
 
 func completeUrls(s []string, mainUrl string) ([]string, error) {
-        u, err := url.Parse(mainUrl)
-        if err != nil {
-                return nil, err
-        }
+	u, err := url.Parse(mainUrl)
+	if err != nil {
+		return nil, err
+	}
 
-        for i, _ := range s {
-                if strings.HasPrefix(s[i], "//") {
-                        s[i] = u.Scheme + ":" + s[i]
-                } else if strings.HasPrefix(s[i], "/") && string(s[i][1]) != "/" {
-                        s[i] = u.Scheme + "://" + u.Host + s[i]
-                } else if !strings.HasPrefix(s[i], "http://") && !strings.HasPrefix(s[i], "https://") {
-                        s[i] = u.Scheme + "://" + u.Host + u.Path + "/" + s[i]
-                }
-        }
-        return s, nil
+	for i := range s {
+		if strings.HasPrefix(s[i], "//") {
+			s[i] = u.Scheme + ":" + s[i]
+		} else if strings.HasPrefix(s[i], "/") && string(s[i][1]) != "/" {
+			s[i] = u.Scheme + "://" + u.Host + s[i]
+		} else if !strings.HasPrefix(s[i], "http://") && !strings.HasPrefix(s[i], "https://") {
+			s[i] = u.Scheme + "://" + u.Host + u.Path + "/" + s[i]
+		}
+	}
+	return s, nil
 }
